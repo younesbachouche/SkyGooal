172734bb88f91d72f7af9ec847d4ddc727681b7a
Younes Bachouche <younesbachouche1@gmail.com>
2026-01-31 17:37:59 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -467,9 +467,9 @@ function getMatchData() {
       competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/55.png",
       competitionName: "Serie A",
       matchTime: '2026-01-31T18:00:00',
-      streamUrlEnglish: "",
+      streamUrlEnglish: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/11_.m3u8",
       streamUrlArabic: "https://sireli1307-be.hf.space/b/p/FOX-DEPORTES/index.m3u8",
-      streamUrlServer3: "",
+      streamUrlServer3: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/12_.m3u8",
       streamUrlServer4: ""
     },  
    
