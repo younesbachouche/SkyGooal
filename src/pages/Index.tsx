6eadb0d0a9319b5722e48eecc9b937a65e7a901c
Younes Bachouche <younesbachouche1@gmail.com>
2026-02-07 15:10:03 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -382,7 +382,7 @@ function getMatchData() {
       matchTime: '2026-02-07T15:30:00',
       streamUrlEnglish: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/67_.m3u8",
       streamUrlArabic: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/41_.m3u8",
-      streamUrlServer3: "",
+      streamUrlServer3: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/19_.m3u8",
       streamUrlServer4: ""
     }, 
     
