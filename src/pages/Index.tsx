8339a167fc5e6961b0caacf1743101d4f2521e4d
Younes Bachouche <younesbachouche1@gmail.com>
2026-02-08 19:42:37 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -373,6 +373,45 @@ function getMatchData() {
       streamUrlServer3: "https://liveua.score806.cc/event2/index.m3u8",
       streamUrlServer4: ""
     }, 
+     {
+      team1: { name: "PSG", logo: "https://imgs.ysscores.com/teams/128/4461690287785.png" },
+      team2: { name: "OM", logo: "https://imgs.ysscores.com/teams/128/6031690287269.png" },
+      competitionLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/55.png",
+      competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/55.png",
+      competitionName: "Ligue 1",
+      matchTime: '2026-02-08T20:45:00',
+      streamUrlEnglish: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/67_.m3u8",
+      streamUrlArabic: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/41_.m3u8",
+      streamUrlServer3: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/19_.m3u8",
+      streamUrlServer4: ""
+    }, 
+    
+     {
+      team1: { name: "Juventus", logo: "https://imgs.ysscores.com/teams/128/9331690283003.png" },
+      team2: { name: "Lazio", logo: "https://imgs.ysscores.com/teams/128/5111690283002.png" },
+      competitionLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/54.png",
+      competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/54.png",
+      competitionName: "Serie A",
+      matchTime: '2026-02-08T20:45:00',
+      streamUrlEnglish: "https://d37w970r8uelg1.cloudfront.net/status/1/fronts.woff2?1f7df6",
+      streamUrlArabic: "https://d2gvljzyfaudw2.cloudfront.net/svg/n1/javascript.json",
+      streamUrlServer3: "https://sireli1307-be.hf.space/b/p/EPL-02/index.m3u8",
+      streamUrlServer4: "https://mocef38798-be.hf.space/b/p/CANAL-LIVE-5/index.m3u8"
+    }, 
+   
+
+     {
+      team1: { name: "Valencia", logo: "https://imgs.ysscores.com/teams/128/7881690196747.png" },
+      team2: { name: "Real Madrid", logo: "https://imgs.ysscores.com/teams/128/1871690196746.png" },
+      competitionLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/87.png",
+      competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/87.png",
+      competitionName: "La Liga",
+      matchTime: '2026-02-08T21:00:00',
+      streamUrlEnglish: "https://d1lgd3xdc194i4.cloudfront.net/status/6/fronts.woff2?60de61",
+      streamUrlArabic: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/76_.m3u8",
+      streamUrlServer3: "https://d2j7633p2yhdob.cloudfront.net/svg/n6/javascript.json",
+      streamUrlServer4: "https://pub-41becd24fbe14338b1f8ff15149e2cf7.r2.dev/37_.m3u8"
+    },  
     
   ];
   
