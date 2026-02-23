9a7e1bc0cbc53cd12e78a0cb0202a9a1763f4386
Younes Bachouche <younesbachouche1@gmail.com>
2026-02-23 22:01:22 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -371,8 +371,8 @@ function getMatchData() {
       matchTime: '2026-02-23T21:00:00',
       streamUrlEnglish: "https://storage.googleapis.com/fangazett/mux_video_ts1/index-1.m3u8",
       streamUrlArabic: "https://d1pds5gq7p4s0a.cloudfront.net/status/1/fronts.woff2?6dff72",
-      streamUrlServer3: "https://cdn.zicotv.cc/dazn-1/index.m3u8",
-      streamUrlServer4: "https://cdn.zicotv.cc/dazn-1/index.m3u8"
+      streamUrlServer3: "https://mando.mohamedabomalek632.workers.dev/?url=http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8",
+      streamUrlServer4: "https://ioswatch.xyz/ios/embed_view.php?id=6dcd08803db4e29b"
     }, 
        
     
