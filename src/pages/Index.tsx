3ffec178c7100325be4930285b121c9608eb03f5
Younes Bachouche <younesbachouche1@gmail.com>
2026-03-02 21:02:51 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -299,7 +299,7 @@ function getMatchData() {
       competitionName: "Serie A",
       matchTime: '2026-03-02T20:45:00',
       streamUrlEnglish: "https://live.alahly-eg.com/live/channels/dlhd?id=ElevenSports2PL",
-      streamUrlArabic: "https://cdn.zicotv.cc/dazn-liga-1/index.m3u8",
+      streamUrlArabic: "",
       streamUrlServer3: "",
       streamUrlServer4: ""
     },
@@ -313,9 +313,9 @@ function getMatchData() {
       competitionName: "La Liga",
       matchTime: '2026-03-02T21:00:00',
       streamUrlEnglish: "https://flowermahi.sbs/svg/n1/javascript.json",
-      streamUrlArabic: "https://yaewe.mohamedabomalek632.workers.dev/?url=http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8",
+      streamUrlArabic: "https://cdn.zicotv.cc/dazn-liga-1/index.m3u8",
       streamUrlServer3: "https://cluone.dad/live/388FAA8743C4E2980F7B4089B7E81087/5.m3u8",
-      streamUrlServer4: "http://sportkn.store/BEIN-SPO---1/video.m3u8"
+      streamUrlServer4: "https://yaewe.mohamedabomalek632.workers.dev/?url=http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8"
     },
     
   
