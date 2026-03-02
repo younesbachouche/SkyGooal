7669920b04061648a2d3ad928229f9143f266b68
Younes Bachouche <younesbachouche1@gmail.com>
2026-03-02 21:04:44 +0100

    Swap stream URLs for servers 3 and 4

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -314,8 +314,8 @@ function getMatchData() {
       matchTime: '2026-03-02T21:00:00',
       streamUrlEnglish: "https://flowermahi.sbs/svg/n1/javascript.json",
       streamUrlArabic: "https://cdn.zicotv.cc/dazn-liga-1/index.m3u8",
-      streamUrlServer3: "https://cluone.dad/live/388FAA8743C4E2980F7B4089B7E81087/5.m3u8",
-      streamUrlServer4: "https://yaewe.mohamedabomalek632.workers.dev/?url=http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8"
+      streamUrlServer3: "https://yaewe.mohamedabomalek632.workers.dev/?url=http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8",
+      streamUrlServer4: "https://cluone.dad/live/388FAA8743C4E2980F7B4089B7E81087/5.m3u8"
     },
     
   
