fd35b7c3c0789588e8b4829c9ca25c2f885b38f6
Younes Bachouche <younesbachouche1@gmail.com>
2026-03-22 13:28:17 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -306,9 +306,9 @@ function getMatchData() {
       competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/87.png",
       competitionName: "La Liga",
       matchTime: '2026-03-22T14:00:00',
-      streamUrlEnglish: "https://storage.googleapis.com/uncetrunvover4/mux_video_ts1/index-1.m3u8",
-      streamUrlArabic: "https://daffodil.xn--1000-ugoa0hsb9a0hb.com/dep.m3u8",
-      streamUrlServer3: "",
+      streamUrlEnglish: "https://d1r7gmumpv7lw0.cloudfront.net/status/5/fronts.woff2?c77c5b",
+      streamUrlArabic: "https://totednffs.store/default/2/default.default",
+      streamUrlServer3: "https://smarthard.click/hls/sabrouchespndeportes/index.m3u8",
       streamUrlServer4: ""
     },
 
