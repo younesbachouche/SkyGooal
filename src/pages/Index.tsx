16c2a835811280259ebd92db32cc35493307bdd6
Younes Bachouche <younesbachouche1@gmail.com>
2026-03-22 14:25:13 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -306,7 +306,7 @@ function getMatchData() {
       competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/87.png",
       competitionName: "La Liga",
       matchTime: '2026-03-22T14:00:00',
-      streamUrlEnglish: "https://m3u8proxy-b66.pages.dev/dlstream?id=71",
+      streamUrlEnglish: "https://playersbassam.blogspot.com/p/bein1pro.html",
       streamUrlArabic: "https://smarthard.click/hls/sabrouchespndeportes/index.m3u8",
       streamUrlServer3: "https://liveua.score806.cc/pt_elevensport2/index.m3u8",
       streamUrlServer4: "http://sportook.online/BEIN-S1/tracks-v1a1/mono.m3u8"
