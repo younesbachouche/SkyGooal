10edd79028a4f20fe70ad67b075dbfbaa34da8a7
Younes Bachouche <younesbachouche1@gmail.com>
2026-04-08 20:13:19 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -279,7 +279,7 @@ function getMatchData() {
       competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/42.png",
       competitionName: "Quarter Final - CHAMPIONS LEAGUE",
       matchTime: '2026-04-08T20:00:00',
-      streamUrlEnglish: "https://yskylive.netlify.app/bein2",
+      streamUrlEnglish: "https://yskylive.netlify.app/bein1",
       streamUrlArabic: "https://storage.googleapis.com/uhgertyobrixaovo1/mux_video_ts1/index-1.m3u8",
       streamUrlServer3: "https://d3h8gvxs6hajby.cloudfront.net/status/1/fronts.woff2?0261bc",
       streamUrlServer4: "https://602.simplyataide.boutique/tnt.m3u8"
@@ -294,7 +294,7 @@ function getMatchData() {
       competitionDarkLogo: "https://images.fotmob.com/image_resources/logo/leaguelogo/dark/42.png",
       competitionName: "Quarter Final - CHAMPIONS LEAGUE",
       matchTime: '2026-04-08T20:00:00',
-      streamUrlEnglish: "https://yskylive.netlify.app/bein1",
+      streamUrlEnglish: "https://yskylive.netlify.app/bein2",
       streamUrlArabic: "https://storage.googleapis.com/uhgertyobrixaovo3/mux_video_ts1/index-1.m3u8",
       streamUrlServer3: "https://d3h8gvxs6hajby.cloudfront.net/status/2/fronts.woff2?84e3b4",
       streamUrlServer4: "https://is.gd/QKWLVe.m3u8"
