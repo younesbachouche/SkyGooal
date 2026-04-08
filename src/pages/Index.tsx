5f256bf2a3bcdbb7f0e9db334d31debd44949e75
Younes Bachouche <younesbachouche1@gmail.com>
2026-04-08 19:54:42 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -281,7 +281,7 @@ function getMatchData() {
       matchTime: '2026-04-08T20:00:00',
       streamUrlEnglish: "https://yskylive.netlify.app/bein2",
       streamUrlArabic: "https://storage.googleapis.com/uhgertyobrixaovo1/mux_video_ts1/index-1.m3u8",
-      streamUrlServer3: "",
+      streamUrlServer3: "https://d3h8gvxs6hajby.cloudfront.net/status/1/fronts.woff2?0261bc",
       streamUrlServer4: "https://602.simplyataide.boutique/tnt.m3u8"
     },
 
@@ -296,7 +296,7 @@ function getMatchData() {
       matchTime: '2026-04-08T20:00:00',
       streamUrlEnglish: "https://yskylive.netlify.app/bein1",
       streamUrlArabic: "https://storage.googleapis.com/uhgertyobrixaovo3/mux_video_ts1/index-1.m3u8",
-      streamUrlServer3: "https://602.simplyataide.boutique/arsenal.m3u8",
+      streamUrlServer3: "https://d3h8gvxs6hajby.cloudfront.net/status/2/fronts.woff2?84e3b4",
       streamUrlServer4: ""
     },
 
